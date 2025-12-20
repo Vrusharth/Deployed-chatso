9c24ee88265f13d199e4c114bedade2d2cd6da94
Vrusharth <vrusharth2003@gmail.com>
2025-12-20 09:54:39 +0530

    Sign up route and jwt token implemented

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router=express.Router()
 
-router.post("/signup",(req,res)=> {
-    res.send("Signup route");
-})
+router.post("/signup",signup)
 
-router.post("/login",(req,res)=> {
-    res.send("Login route");
-})
+router.post("/login",login)
 
-router.post("/logout",(req,res)=> {
-    res.send("Logout route");
-})
+router.post("/logout",logout)
+
 export default router
