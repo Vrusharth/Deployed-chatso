9503a98164dba6a9a2e6e37e29fd94322e418367
Vrusharth <vrusharth2003@gmail.com>
2025-12-31 15:40:32 +0530

    Final optimizations done

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,10 @@ import { connectDB } from "./lib/db.js";
 import messageRoutes from "./routes/message.route.js"
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { app, server } from "./lib/socket.js";
 
-const app=express();
+dotenv.config()
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
@@ -18,9 +20,9 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 
 
-dotenv.config()
+
 const PORT= process.env.PORT
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log("Server is running on port " , PORT);
     connectDB()    
 })
--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,45 +1,67 @@
-import {create} from "zustand"
+import { create } from "zustand"
 import { axiosInstance } from "../lib/axios";
-import {toast} from "react-hot-toast"
-export const useChatStore = create((set,get)=>({
-    messages: [],
-    users:[],
-    selectedUser:null,
-    isUsersLoading:false,
-    isMessagesLoading:false,
+import { toast } from "react-hot-toast"
+import { useAuthStore } from "./useAuthStore"
 
-    getUsers: async()=> {
-        set({isUsersLoading:true});
-        try {
-            const res = await axiosInstance.get("/messages/users")
-            set({users:res.data})
-        } catch (error) {
-            toast.error(error.response.data.message);
-        }finally{
-            set({isUsersLoading:false})
-        }
-    },
-    getMessages: async (userId) => {
-        set({ isMessagesLoading: true });
-        try {
-          const res = await axiosInstance.get(`/messages/${userId}`);
-          set({ messages: res.data });
-        } catch (error) {
-          toast.error(error.response.data.message);
-        } finally {
-          set({ isMessagesLoading: false });
-        }
-    },
-    sendMessage: async (messageData) => {
+export const useChatStore = create((set, get) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
 
-      const {selectedUser,messages} = get();
-      try {
-        const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
-        set({messages:[...messages,res.data]})
-      } catch (error) {
-        toast.error(error.response.data.message)
-      }
-    },
-          
-      setSelectedUser: (selectedUser) => set ({selectedUser}),
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users")
+      set({ users: res.data })
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUsersLoading: false })
+    }
+  },
+  getMessages: async (userId) => {
+    set({ isMessagesLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isMessagesLoading: false });
+    }
+  },
+  sendMessage: async (messageData) => {
+
+    const { selectedUser, messages } = get();
+    try {
+      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
+      set({ messages: [...messages, res.data] })
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+  },
+
+  subscribeToMessages: () => {
+    const { selectedUser } = get()
+    if (!selectedUser) return;
+
+    const socket = useAuthStore.getState().socket;
+
+    socket.on("newMessage", (newMessage) => {
+      if(newMessage.senderId !== selectedUser._id) return;
+      set({
+        messages: [...get().messages, newMessage],
+      })
+    })
+  },
+
+  unsubscribeFromMessages: ()=>{
+    const socket = useAuthStore.getState().socket;
+    socket.off("newMessage")
+  },
+
+  setSelectedUser: (selectedUser) => set({ selectedUser }),
+
 }))
